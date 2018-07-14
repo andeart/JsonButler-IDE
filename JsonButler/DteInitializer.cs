50ef1e9281a85a825f07b5b04784889b41c8221f
Anurag Devanapally <anurag.devanapally@gmail.com>
2018-07-13 22:19:37 -0700

    First commit. Serialize Type functionality implemented.

---
+++ JsonButler/DteInitializer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using EnvDTE80;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+
+
+namespace Andeart.JsonButler
+{
+
+    internal class DteInitializer : IVsShellPropertyEvents
+    {
+        private IVsShell _shellService;
+        private uint _cookie;
+        private Action _callback;
+
+        public DteInitializer (IVsShell shellService, Action callback)
+        {
+            _shellService = shellService;
+            _callback = callback;
+
+            // Set an event handler to detect when the IDE is fully initialized
+            int handlerResult = _shellService.AdviseShellPropertyChanges (this, out _cookie);
+
+            ErrorHandler.ThrowOnFailure (handlerResult);
+        }
+
+        int IVsShellPropertyEvents.OnShellPropertyChange (int propid, object var)
+        {
+            if (propid != (int) __VSSPROPID.VSSPROPID_Zombie)
+            {
+                return VSConstants.S_OK;
+            }
+
+            bool isZombie = (bool) var;
+
+            if (isZombie)
+            {
+                return VSConstants.S_OK;
+            }
+
+            // Release the event handler to detect when the IDE is fully initialized
+            int handlerResult = _shellService.UnadviseShellPropertyChanges (_cookie);
+
+            ErrorHandler.ThrowOnFailure (handlerResult);
+
+            _cookie = 0;
+            _callback.Invoke ();
+
+            return VSConstants.S_OK;
+        }
+    }
+
+}
---
+++ JsonButler/DteInitializerFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+
+
+namespace Andeart.JsonButler
+{
+
+    internal class DteInitializerFactory
+    {
+        private static Action<DTE2> _onDteInitialized;
+
+        private static Package _package;
+        private static DteInitializer _dteInitializer;
+
+        private static IServiceProvider ServiceProvider => _package;
+
+        public static void Initialize (Package package, Action<DTE2> OnDteInitialized)
+        {
+            _package = package ?? throw new ArgumentNullException(nameof(package));
+            _onDteInitialized = OnDteInitialized;
+            Initialize ();
+        }
+
+        private static void Initialize ()
+        {
+            DTE2 dte = ServiceProvider.GetService (typeof(SDTE)) as DTE2;
+            _onDteInitialized?.Invoke (dte);
+
+            if (dte == null) // The IDE is not yet fully initialized
+            {
+                IVsShell shellService = ServiceProvider.GetService (typeof(SVsShell)) as IVsShell;
+                _dteInitializer = new DteInitializer (shellService, Initialize);
+            } else
+            {
+                _dteInitializer = null;
+            }
+        }
+    }
+
+}
