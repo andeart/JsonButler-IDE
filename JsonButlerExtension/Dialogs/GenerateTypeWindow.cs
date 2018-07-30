34c40452e7905233f8d2710f91c15c0724bd53cc
Anurag Devanapally <anurag.devanapally@gmail.com>
2018-07-30 00:22:55 -0700

    CodeGeneration approach refactored (now uses Roslyn). Solution/projects naming updated to JsonButlerExtension. Windows form implemented to help type generation. Code clean-up/reorganisation.

---
+++ JsonButlerExtension/Dialogs/GenerateTypeWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+
+namespace Andeart.JsonButlerIde.Dialogs
+{
+
+    public partial class GenerateTypeWindow : Form
+    {
+        public string TypeNamespace { get; private set; }
+        public string TypeName { get; private set; }
+
+        public GenerateTypeWindow ()
+        {
+            InitializeComponent ();
+        }
+
+        private void ButtonGenerate_Click (object sender, EventArgs e)
+        {
+            TypeNamespace = textNamespace.Text;
+            TypeName = textName.Text;
+            DialogResult = DialogResult.OK;
+            Close ();
+        }
+
+        private void TextNamespace_GotFocus (object sender, EventArgs e)
+        {
+            textNamespace.Text = string.Empty;
+            textNamespace.ForeColor = SystemColors.WindowText;
+        }
+
+        private void TextName_GotFocus (object sender, EventArgs e)
+        {
+            textName.Text = string.Empty;
+            textName.ForeColor = SystemColors.WindowText;
+        }
+    }
+
+}
