6c61911840b1b5f826411627444985e511f6037e
Anurag D <andymations.ad@gmail.com>
2019-03-03 16:47:50 -0800

    Update build/test script

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -8,11 +8,14 @@ if not "%configurationName%"=="Release" (
 )
 
 nuget restore ./JsonButlerIde/JsonButlerIde.sln
+dotnet clean ./JsonButlerIde/JsonButlerIde/JsonButlerIde.csproj
 
 REM A bit of a hassle to ensure we're always calling the latest MSBuild available on the system.
 REM Especially true in our case because of this error: https://social.msdn.microsoft.com/Forums/vstudio/en-US/fd220999-5761-475a-bf86-98dff6b35218/unable-to-compile-vsix-project-that-is-a-part-of-my-solution-using-amd64-msbuild-from-vs2015?forum=msbuild
 for /f "usebackq tokens=*" %%m in (`"%~dp0get-msBuild-path.bat"`) do (
-    call %%m ./JsonButlerIde/JsonButlerIde.sln /p:Configuration=%configurationName% 
+    call %%m ./JsonButlerIde/JsonButlerIde.sln -p:Configuration=%configurationName% 
 )
 
-python ./Scripts/test.py "./JsonButlerIde/JsonButlerIde/bin/%configurationName%/JsonButlerIde.vsix"
+if ("%runTests%"=="true") (
+    python ./Scripts/test.py "./JsonButlerIde/JsonButlerIde/bin/%configurationName%/JsonButlerIde.vsix"
+)
