298f018fc30c8e395ba660df9a68d7746f4ae197
Anurag D <andymations.ad@gmail.com>
2019-03-03 16:41:50 -0800

    Update AssemblyInfo; Release 1.0.0.0

--- JsonButlerIde/JsonButlerIde/Properties/AssemblyInfo.cs
+++ JsonButlerIde/JsonButlerIde/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("JsonButlerIde")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle("Andeart.JsonButlerIde")]
+[assembly: AssemblyDescription ("Visual Studio extension that adds several JSON-conversion features within the IDE. Generate C# types from JSON. Serialize C# types to JSON (within code editor). Change cases of phrases.")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Andeart")]
-[assembly: AssemblyProduct ("JsonButlerIde")]
+[assembly: AssemblyProduct("Andeart.JsonButlerIde")]
 [assembly: AssemblyCopyright ("Copyright © 2018 ANURAG DEVANAPALLY")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
@@ -18,15 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible (false)]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.11.1.0")]
